300fe0b0497280bb502a8641141761e36c80b446
Shrey Bagroy <sbagroy96@gmail.com>
2024-01-20 01:04:49 -0500

    Initializing chrome extension code

--- sanitized-curl-command.sh
+++ sanitized-curl-command.sh
@@ -1,4 +1,4 @@
-This is the cURL command that the browser extension will need to emulate to get the content of unread emails:
+## This is the cURL command that the browser extension will need to emulate to get the content of unread emails:
 
 curl 'https://mail.google.com/sync/u/0/i/fd?hl=en&c=0&rt=r&pt=ji' --compressed -X POST \
 -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0' \
@@ -16,7 +16,7 @@ curl 'https://mail.google.com/sync/u/0/i/fd?hl=en&c=0&rt=r&pt=ji' --compressed -
 --data-raw '[sanitized]'
 
 
-The data-raw format is [[["thread-f:12345678912312312",1,null,null,1]],1]. This is (likely) the field where the POST call gets parameterized by the email identifier.
+## The data-raw format is [[["thread-f:12345678912312312",1,null,null,1]],1]. This is (likely) the field where the POST call gets parameterized by the email identifier.
 
 
-Note: This is as of 01/19/2024, keeping consistent with the API changes is likely going to be the hardest piece of keeping this operational.
+## Note: This is as of 01/19/2024, keeping consistent with the API changes is likely going to be the hardest piece of keeping this operational.
